f5e15f5ae5804566a6d447a5ad782ab7c27e920c
AyaTouicha <117740707+aya2705@users.noreply.github.com>
2024-06-24 18:40:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var sp,btn_start,btn_stop,t,ms,s,mn,h;
 
 
-//todo: fonction pour initialiser les variables quand la page se charge
 
 window.onload=function(){
   sp = document.getElementsByTagName('span');
@@ -15,7 +14,6 @@ btn_stop.addEventListener('click',function(){
   alert("vous avez cliquer sur start");
 })
 
-//todo: mettre en place le compteur
 
 function update_chronometre(){
   ms+=1;
@@ -32,7 +30,6 @@ function update_chronometre(){
     h+=1;
   }
   
-  //todo = insertion des valeurs dans les spans [0] permet de selectionner le premier span
 
   sp[0].innerHTML=h + "h";
   sp[1].innerHTML=mn +"min";
@@ -40,7 +37,6 @@ function update_chronometre(){
   sp[3].innerHTML=ms +"ms";
 }
 
-//todo: mettre en place la fonction du button start
 
 function start() {
   t = setInterval(update_chronometre, 100);
@@ -49,14 +45,14 @@ function start() {
 
 
 
-//todo:stopper le chronometre
+
 
 function stop() {
   clearInterval(t); //?suppression de l'interval t que nous avions creer
   btn_start.disabled = false; 
 }
 
-//todo:initialiser les valeurs du compteur a 0
+
 
 function restart() {
   clearInterval(t);
@@ -65,7 +61,7 @@ function restart() {
   s = 0;
   mn = 0;
   h = 0;
-  // Insert the new values into the spans
+
 
   sp[0].innerHTML = h + " h";
   sp[1].innerHTML = mn + " min";
